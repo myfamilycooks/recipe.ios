947319cf3f8c23e7e7138cc1217be2ad69b5d340
Shaun Sargent <ShaunM10@gmail.com>
2016-09-29 19:59:59 -0400

    Initial Project commit.

---
+++ recipe.ios/RecipeDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RecipeDetailViewController.swift
+//  recipe.ios
+//
+//  Created by Shaun Sargent on 9/29/16.
+//  Copyright © 2016 My Family Cooks. All rights reserved.
+//
+
+import UIKit
+
+class RecipeDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
